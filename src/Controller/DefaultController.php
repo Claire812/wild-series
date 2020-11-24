3622aaeea9fcf9f64afe68af6189dd24aa3dd914
Claire812 <clairedaudignon@gmail.com>
2020-11-24 09:07:35 +0100

    quest 1 done

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig',[
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
