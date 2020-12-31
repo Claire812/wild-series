1452e50b04c29045e71a6dcc9d1c5eb240975be0
Claire812 <clairedaudignon@gmail.com>
2020-12-31 11:34:06 +0100

    searchbar done and quest 22 done

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
-file for video quest 20
-https://www.loom.com/share/d8dc822fd3c941fc839796f55ccb318c
+file for video quest 22
+https://www.loom.com/share/e1effeeb985c413d842c0aa696e2390f
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -10,6 +10,8 @@ use App\Entity\Program;
 use App\Entity\Season;
 use App\Form\CommentType;
 use App\Form\ProgramType;
+use App\Form\SearchProgramType;
+use App\Repository\ProgramRepository;
 use App\Service\Slugify;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,13 +33,23 @@ class ProgramController extends AbstractController
      * @Route("/", name="index")
      * @return Response A response instance
      */
-    public function index(): Response
+    public function index(Request $request, ProgramRepository $programRepository): Response
     {
-        $programs = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findAll();
+        $form = $this->createForm(SearchProgramType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()['search'];
+            $programs = $programRepository->findLikeName($search);
+        } else {
+            $programs = $programRepository->findAll();
+        }
+
+
+
+
         return $this->render('program/index.html.twig', [
             'programs' => $programs,
+            'form' => $form->createView(),
         ]);
     }
 
